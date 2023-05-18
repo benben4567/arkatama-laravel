2357d6e3d347d4235b5d6989bcd6d11208c6f859
benben4567 <benben4567@gmail.com>
2023-05-18 12:05:34 +0700

    initial commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return view('user.index');
+    }
+
+    public function create()
+    {
+        return 'Ini halaman create';
+    }
+}
