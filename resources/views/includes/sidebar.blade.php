3a13de9f30dc1a7374aeb2dea77a2ce8f2267bdb
benben4567 <benben4567@gmail.com>
2023-05-27 11:08:29 +0700

    fix: add menu slider

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -6,6 +6,12 @@
                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                 Dashboard
             </a>
+            <div class="sb-sidenav-menu-heading">Content</div>
+            <a class="nav-link" href="{{ route('slider.index') }}">
+                <div class="sb-nav-link-icon"><i class="fas fa-image"></i></div>
+                Slider
+            </a>
+
             <div class="sb-sidenav-menu-heading">Management</div>
             <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
                 <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
