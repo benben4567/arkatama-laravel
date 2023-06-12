5a8b3625c5ae6a7137a7f2f89ce6cb4e6bfe3718
Benben Bagus P <benben4567@gmail.com>
2023-06-12 19:58:48 +0700

    fix: image

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -37,7 +37,12 @@
     <section class="py-5">
         <div class="container px-4 px-lg-5 my-5">
             <div class="row gx-4 gx-lg-5 align-items-center">
-                <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="{{ asset('storage/product/' . $product->image) }}" alt="..." /></div>
+                @if ($product->image)
+                    <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="{{ asset('storage/product/' . $product->image) }}" alt="{{$product->image}}" /></div>
+                @else
+                    <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="{{ asset('images/default-product-detail.png') }}" alt="default-image" /></div>
+                @endif
+                
                 <div class="col-md-6">
                     <div class="small mb-1">SKU: BST-498</div>
                     <h1 class="display-5 fw-bolder">{{ $product->name }}</h1>
@@ -67,7 +72,11 @@
                     <div class="col mb-5">
                         <div class="card h-100">
                             <!-- Product image-->
-                            <img class="card-img-top" src="{{ asset('storage/product/' . $product->image) }}" alt="..." />
+                            @if ($product->image)
+                                <img class="card-img-top" src="{{ asset('storage/product/' . $product->image) }}" alt="..." />
+                            @else
+                                <img class="card-img-top" src="{{ asset('images/default-product.png') }}" alt="..." />
+                            @endif
                             <!-- Product details-->
                             <div class="card-body p-4">
                                 <div class="text-center">
